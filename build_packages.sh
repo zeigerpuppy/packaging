2a06339002b840b10b0a6c7381e3704159b21ffb
Jason Petersen <jason@citusdata.com>
2016-05-09 17:54:33 -0600

    Add GITHUB_TOKEN to build

--- build_packages.sh
+++ build_packages.sh
@@ -30,13 +30,13 @@ while read line; do
     mkdir -p "${outputdir}"
 
     if [[ "${os}" = 'debian' ]] || [[ "${os}" = 'ubuntu' ]]; then
-        docker run --rm -v ${outputdir}:/packages -e "DEBFULLNAME=${name}" -e "DEBEMAIL=${email}" citusdata/buildbox-${os}:${release} citus "$buildtype"
+        docker run --rm -v ${outputdir}:/packages -e "GITHUB_TOKEN=${GITHUB_TOKEN}" -e "DEBFULLNAME=${name}" -e "DEBEMAIL=${email}" citusdata/buildbox-${os}:${release} citus "$buildtype"
     elif [[ "${os}" = 'centos' ]] || [[ "${os}" = 'fedora' ]] || [[ "${os}" = 'oraclelinux' ]]; then
         # redhat variants need to build each PostgreSQL version separately
         IFS=' '
         for pgversion in ${pgversions}; do
             pgshort=${pgversion//./}
-            docker run --rm -v ${outputdir}:/packages -e "RPM_PACKAGER=${packager}" citusdata/buildbox-${os}-${pgshort}:${release} citus "$buildtype"
+            docker run --rm -v ${outputdir}:/packages -e "GITHUB_TOKEN=${GITHUB_TOKEN}" -e "RPM_PACKAGER=${packager}" citusdata/buildbox-${os}-${pgshort}:${release} citus "$buildtype"
         done
     else
         echo "$0: unrecognized OS -- ${os}" >&2
