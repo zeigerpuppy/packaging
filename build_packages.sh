ace51e8d741f9f29c641d5562b8ea4693419e70f
Jason Petersen <jason@citusdata.com>
2016-05-10 16:34:21 -0600

    Permit arbitrary projects

--- build_packages.sh
+++ build_packages.sh
@@ -10,12 +10,13 @@ topdir=`pwd`
 packagesdir=${topdir}/packages
 badusage=64
 
-if [ "$#" -ne 1 ]; then
-    echo "$0: build type required" >&2
+if [ "$#" -ne 2 ]; then
+    echo "$0: project and build type required" >&2
     exit $badusage
 fi
 
-buildtype=$1
+project=$1
+buildtype=$2
 
 name=`git config --get user.name`
 email=`git config --get user.email`
@@ -30,13 +31,13 @@ while read line; do
     mkdir -p "${outputdir}"
 
     if [[ "${os}" = 'debian' ]] || [[ "${os}" = 'ubuntu' ]]; then
-        docker run --rm -v ${outputdir}:/packages -e "GITHUB_TOKEN=${GITHUB_TOKEN}" -e "DEBFULLNAME=${name}" -e "DEBEMAIL=${email}" citusdata/buildbox-${os}:${release} citus "$buildtype"
+        docker run --rm -v ${outputdir}:/packages -e "GITHUB_TOKEN=${GITHUB_TOKEN}" -e "DEBFULLNAME=${name}" -e "DEBEMAIL=${email}" citusdata/buildbox-${os}:${release} "$project" "$buildtype"
     elif [[ "${os}" = 'centos' ]] || [[ "${os}" = 'fedora' ]] || [[ "${os}" = 'oraclelinux' ]]; then
         # redhat variants need to build each PostgreSQL version separately
         IFS=' '
         for pgversion in ${pgversions}; do
             pgshort=${pgversion//./}
-            docker run --rm -v ${outputdir}:/packages -e "GITHUB_TOKEN=${GITHUB_TOKEN}" -e "RPM_PACKAGER=${packager}" citusdata/buildbox-${os}-${pgshort}:${release} citus "$buildtype"
+            docker run --rm -v ${outputdir}:/packages -e "GITHUB_TOKEN=${GITHUB_TOKEN}" -e "RPM_PACKAGER=${packager}" citusdata/buildbox-${os}-${pgshort}:${release} "$project" "$buildtype"
         done
     else
         echo "$0: unrecognized OS -- ${os}" >&2
