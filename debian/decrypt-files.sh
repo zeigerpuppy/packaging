517cc48ff9ac5f189e68c2c74e9edfa63ceadf72
Jelte Fennema <github-tech@jeltef.nl>
2020-05-07 19:07:27 +0200

    Modify encryption text to match pg_auto_failover

--- debian/decrypt-files.sh
+++ debian/decrypt-files.sh
@@ -24,16 +24,16 @@ the software from Microsoft or its licensed distributors.
 BY USING THE SOFTWARE, YOU ACCEPT THIS AGREEMENT.
 "
 
-CITUS_ACCEPT_LICENSE="${CITUS_ACCEPT_LICENSE:-}"
+PGAUTOFAILOVER_ACCEPT_LICENSE="${PGAUTOFAILOVER_ACCEPT_LICENSE:-}"
 
 interactive_license=false
-while [ -z "$CITUS_ACCEPT_LICENSE" ]; do
+while [ -z "$PGAUTOFAILOVER_ACCEPT_LICENSE" ]; do
     interactive_license=true
     echo "Do you accept these terms? YES/NO"
-    read -r CITUS_ACCEPT_LICENSE
+    read -r PGAUTOFAILOVER_ACCEPT_LICENSE
 done
 
-case "$CITUS_ACCEPT_LICENSE" in
+case "$PGAUTOFAILOVER_ACCEPT_LICENSE" in
     YES );;
     y|Y|Yes|yes )
         echo "ERROR: Only YES is accepted (all capital letters)"
@@ -45,47 +45,46 @@ case "$CITUS_ACCEPT_LICENSE" in
 esac
 
 if [ $interactive_license = false ]; then
-    echo "Accepted terms by using CITUS_ACCEPT_LICENSE=YES environment variable"
+    echo "Accepted terms by using PGAUTOFAILOVER_ACCEPT_LICENSE=YES environment variable"
 fi
 
 encryption_disclaimer_text="
-Since Citus is a distributed database, data is sent over the network between
+Since pg_auto_failover manages failovers, data is sent over the network between
 nodes. It is YOUR RESPONSIBILITY as an operator to ensure that this traffic is
 secure.
 
-Since Citus version 8.1.0 (released 2018-12-17) the traffic between the
-different nodes in the cluster is encrypted for NEW installations. This is done
-by using TLS with self-signed certificates. This means that this does NOT
-protect against Man-In-The-Middle attacks. This only protects against passive
+Since pg_auto_failover version 1.3.0 (released 2020-05-07) the traffic between
+the different nodes in the cluster is encrypted automatically when using the
+--ssl-self-signed flag to create the nodes in the cluster. This is done by
+using TLS with self-signed certificates. This means that this does NOT protect
+against Man-In-The-Middle attacks. This only protects against passive
 eavesdropping on the network.
 
-This automatic TLS setup of self-signed certificates and TLS is NOT DONE in the
-following cases:
-1. The Citus clusters was originally created with a Citus version before 8.1.0.
-   Even when the cluster is later upgraded to version 8.1.0 or higher. This is
-   to make sure partially upgraded clusters continue to work.
-2. The ssl Postgres configuration option is already set to 'on'. This indicates
-   that the operator has set up their own certificates.
+This automatic TLS setup of self-signed certificates and TLS is NOT DONE when
+the cluster was originally created with a pg_auto_failover version before
+1.3.0. Even when the cluster is later upgraded to version 1.3.0 or higher.
+This is to make sure partially upgraded clusters continue to work.
 
-In these cases it is assumed the operator has set up appropriate security
-themselves.
+To enable TLS on these clusters you can use the 'pg_autoctl enable ssl'
+command. It's usage is explained in detail here:
+https://pg-auto-failover.readthedocs.io/en/stable/security.html#enable-ssl-connections-on-an-existing-setup
 
-So, with the default settings Citus clusters are not safe from
+Keep in mind that when using --ssl-self-signed the clusters is not safe from
 Man-In-The-Middle attacks. To secure the traffic completely you need to follow
 the practices outlined here:
-https://docs.citusdata.com/en/stable/admin_guide/cluster_management.html#connection-management
+https://pg-auto-failover.readthedocs.io/en/stable/security.html#using-your-own-ssl-certificates
 
 Please confirm that you have read this and understand that you should set up
 TLS yourself to send traffic between nodes securely:
 YES/NO?"
 
-CITUS_ACCEPT_ENCRYPTION_DISCLAIMER="${CITUS_ACCEPT_ENCRYPTION_DISCLAIMER:-}"
-while [ -z "$CITUS_ACCEPT_ENCRYPTION_DISCLAIMER" ]; do
+PGAUTOFAILOVER_ACCEPT_ENCRYPTION_DISCLAIMER="${PGAUTOFAILOVER_ACCEPT_ENCRYPTION_DISCLAIMER:-}"
+while [ -z "$PGAUTOFAILOVER_ACCEPT_ENCRYPTION_DISCLAIMER" ]; do
     echo "$encryption_disclaimer_text"
-    read -r CITUS_ACCEPT_ENCRYPTION_DISCLAIMER
+    read -r PGAUTOFAILOVER_ACCEPT_ENCRYPTION_DISCLAIMER
 done
 
-case "$CITUS_ACCEPT_ENCRYPTION_DISCLAIMER" in
+case "$PGAUTOFAILOVER_ACCEPT_ENCRYPTION_DISCLAIMER" in
     YES );;
     y|Y|Yes|yes )
         echo "ERROR: Only YES is accepted (all capital letters)"
@@ -98,17 +97,17 @@ esac
 
 # create a temporary directory for gpg to use so it doesn't output warnings
 temp_gnupghome="$(mktemp -d)"
-CITUS_LICENSE_KEY="${CITUS_LICENSE_KEY:-}"
-while [ -z "$CITUS_LICENSE_KEY" ]; do
+PGAUTOFAILOVER_LICENSE_KEY="${PGAUTOFAILOVER_LICENSE_KEY:-}"
+while [ -z "$PGAUTOFAILOVER_LICENSE_KEY" ]; do
     echo ''
     echo 'Please enter license key:'
-    read -r CITUS_LICENSE_KEY
+    read -r PGAUTOFAILOVER_LICENSE_KEY
 done
 
 # Try to decrypt the first file in the list to check if the key is correct
 if ! gpg --output "/dev/null" \
         --batch --no-tty --yes --quiet \
-        --passphrase "$CITUS_LICENSE_KEY" \
+        --passphrase "$PGAUTOFAILOVER_LICENSE_KEY" \
         --homedir "$temp_gnupghome" \
         --decrypt "$(head -n 1 "$secret_files_list").gpg" 2> /dev/null; then
     echo "ERROR: Invalid license key supplied"
@@ -124,7 +123,7 @@ decrypt() {
     # decrypt the encrypted file
     gpg --output "$path_unencrypted" \
         --batch --no-tty --yes --quiet \
-        --passphrase "$CITUS_LICENSE_KEY" \
+        --passphrase "$PGAUTOFAILOVER_LICENSE_KEY" \
         --homedir "$temp_gnupghome" \
         --decrypt "$path_encrypted"
 
